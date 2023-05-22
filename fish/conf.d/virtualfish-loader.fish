d9e0afea73936c8e165dfc8bc8c6f99f1bc54b51
mattias bermell <mattias@modl.ai>
2023-05-22 09:04:13 +0200

    python 3.8 default in virtualfish

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
 set -g VIRTUALFISH_VERSION 2.5.5
-set -g VIRTUALFISH_PYTHON_EXEC /home/mattias/.pyenv/versions/3.9.16/bin/python
-source /home/mattias/.pyenv/versions/3.9.16/lib/python3.9/site-packages/virtualfish/virtual.fish
+set -g VIRTUALFISH_PYTHON_EXEC /home/mattias/.pyenv/versions/3.8.16/bin/python
+source /home/mattias/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
