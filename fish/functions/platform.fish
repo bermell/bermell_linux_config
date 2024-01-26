11ca0d29ee82073d510d66624a7b952a18c1b0fb
mattias bermell <mattias@modl.ai>
2024-01-26 11:07:01 +0100

    fish: fixx the platform function

--- fish/functions/platform.fish
+++ fish/functions/platform.fish
@@ -1,16 +1,13 @@
 function platform --wraps=cd --description 'does docker compose up or down for the platform'
     set -l platform_directory /home/mattias/repos/product-platform-glitchfinder/docker/platform-local
+    set -l profiles --profile jobs_service --profile no-authentication
+
     cd $platform_directory
 
-    if test "$argv[1]" = "up"
-        # set -f _extra_arg "" 
-        # if test (count $argv) -gt 1
-        #   set -f _extra_arg $argv[2]
-        # end
-
-        docker compose --profile jobs_service --profile no-authentication  up --force-recreate --build -d $_extra_arg
-    else if test "$argv[1]" = "down"
-        docker compose down --remove-orphans
+    if test "$argv[1]" = up
+        docker compose $profiles up --force-recreate --build -d
+    else if test "$argv[1]" = down
+        docker compose $profiles down --remove-orphans
     else
         echo 'unknown command'
     end
