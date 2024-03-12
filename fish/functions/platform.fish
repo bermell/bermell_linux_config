d40d42018bc3349ee623db7839bab31ba61f433a
mattias bermell <mattias@modl.ai>
2024-03-12 21:38:04 +0100

    [fish] platform.fish gets updates

--- fish/functions/platform.fish
+++ fish/functions/platform.fish
@@ -1,14 +1,38 @@
 function platform --wraps=cd --description 'does docker compose up or down for the platform'
     set -l platform_directory /home/mattias/repos/platform/docker/platform-local
-    set -l profiles --profile jobs_service --profile no-authentication --profile build
+    set -l profiles --profile jobs_service --profile build
+    set -l auth_profile --profile authentication
+    set -l no_auth_profile --profile no-authentication
+    set -l all_profiles $profiles $auth_profile $no_auth_profile
+
+
+    set -l usage "Usage: platform [up|up-auth|down|watch]"
+
+
+    #if there are no arguments, exit here
+    if test -z "$argv"
+        cd $platform_directory
+        cd ../../
+        return
+    end
+
 
     cd $platform_directory
 
     if test "$argv[1]" = up
-        docker compose $profiles up --force-recreate --build -d
+        set -l compose_args $profiles $no_auth_profile up --force-recreate --build -d
+    else if test "$argv[1]" = up-auth
+        set -l compose_args $profiles $auth_profile up --force-recreate --build -d
     else if test "$argv[1]" = down
-        docker compose $profiles down --remove-orphans
+        set -l compose_args $all_profiles down --remove-orphans
+    else if test "$argv[1]" = watch
+        set -l compose_args $profiles $no_auth_profile watch
+    else if test "$argv[1]" = watch-auth
+        set -l compose_args $profiles $auth_profile watch
     else
-        echo 'unknown command'
+        echo 'unknown command: ' $argv[1]
+        echo $usage
+        return
     end
+    docker compose $compose_args
 end
